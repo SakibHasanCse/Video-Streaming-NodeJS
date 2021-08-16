7a81b379949ac94964fd021d8ab936ccc265bc9d
SakibHasanCse <sakibhasan9641@gmail.com>
2021-08-16 23:54:27 +0600

    upload video

--- server.js
+++ server.js
@@ -3,9 +3,29 @@ var express = require('express'),
     http = require('http'),
     socketIo = require('socket.io'),
     fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    dotenv = require('dotenv'),
+    mongodb = require('mongodb')
 
-const VideoRouter = require('./src/router/videos')
+dotenv.config();
+
+const VideoRouter = require('./src/router/videos');
+const mongo_url = process.env.MONGO_URL;
+
+app.get('/upload-video', (req, res) => {
+    mongodb.MongoClient.connect(mongo_url, (err, client) => {
+        if (error) {
+            res.json(error);
+            return;
+        }
+        const db = client.db('videos');
+        const bucket = new mongodb.GridFSBucket(db);
+        const videoUploadStream = bucket.openDownloadStream('bigbuck');
+        const videoReadStream = bucket.openReadStream('./hdvideo.mp4');
+        videoReadStream.pipe(videoUploadStream);
+        res.status(200).send("Done...");
+    })
+})
 app.use('/', VideoRouter);
 
 const port = process.env.PORT || 8000;
